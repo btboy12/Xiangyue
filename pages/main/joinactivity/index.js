0ca86bf3b0f6f4fc9a0aed654a3046d8cb4e151a
leticiafung <zqzb1111@163.com>
2018-07-14 14:16:04 +0800

    use globalid

--- pages/main/joinactivity/index.js
+++ pages/main/joinactivity/index.js
@@ -14,7 +14,52 @@ Page({
       brief: '一起玩吧啊啊啊啊啊啊啊啊'
     }],
     avatarsrc:'',
-    join:false
+    join:false,
+    userId :''//用户id
+  },
+  //获取活动列表
+  getActivityList: function (){
+    //不提供userid怎么处理
+    userid = getUserId() === -1 ? 0 : getUserId();
+    wx.request({
+      url: '/displayActivity',
+      method:'get',
+      data:getUserId(),
+      success:function(res){
+        if(res){
+          this.setData({
+            activities : res
+          })
+        }else{//没有活动列表
+          wx.showToast({
+            title: '暂时没有活动',
+          })
+        }
+      },
+      fail:function(){
+        wx.showToast({
+          title: '返回列表出错',
+        })
+      }
+      })
+  },
+  //获取用户id
+  getUserId: function () {
+    let id = -1;
+    wx.getStorage({
+      key: 'token',
+      success: function(res) {
+        this.setData({
+          userId : res.data
+        })
+      },
+      fail:function(){
+        false;
+      }
+    })
+  },
+  onShow:function(){
+
   }
 
 })
